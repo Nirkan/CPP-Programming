df52f0308d4bca0606927d9de3acd7328abe7963
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-28 16:10:39 +0200

    variables

---
+++ freeCode-CPP/T3.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string characterName = "Trump";
+    int characterAge;
+    characterAge = 74;
+
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    
+    characterName = "President";
+
+    cout << "He liked the name " << characterName << endl;
+    cout << "But he did not like being " << characterAge << endl;
+
+
+    return 0;
+}
