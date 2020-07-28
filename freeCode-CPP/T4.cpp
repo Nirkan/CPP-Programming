744814295a4c7935f9da7fb81f2baddc89301b55
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-28 16:11:35 +0200

    Data Types

---
+++ freeCode-CPP/T4.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char grade = 'A';                       // single character
+    string phrase = "Times of India";        // Bunch of characters
+    int age = 50;                           // Whole number
+    float pi = 3.14;
+    double gpa = 4.5;
+    bool isMale = true;                     // True False values
+
+    cout << grade << endl;
+    cout << phrase << endl;
+    cout << age << endl;
+    cout << pi << endl;
+    cout << gpa << endl;
+    cout << isMale << endl;
+
+    return 0;
+}
