19c384ec2da5d3e7dbb3df48bec93a1f8cffde80
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-28 19:19:31 +0200

    Numbers&Math

---
+++ freeCode-CPP/T6.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    cout << 40 << endl;
+
+    cout << 5 + 7 << endl;
+
+    cout << 5 - 7 << endl;
+
+    cout << 5 * 7 << endl;
+
+    cout << 5 / 7 << endl;
+
+    cout << 10 % 3 << endl;         // Modulus : gives reaminder
+
+    cout <<  5 + 4 * 10 << endl;
+
+    cout << (5 + 4) * 10 << endl;
+
+    int wnum = 5;
+    double dnum = 5.5;
+
+    wnum++ ;
+
+    cout << wnum << endl;
+
+    wnum-- ;
+
+    cout << wnum << endl;
+
+    wnum += 80;              // +=, -=, *=, /=
+
+    cout << wnum << endl;
+
+
+    cout << 5.5 + 9 << endl;
+
+    cout << 10 / 3 << endl;
+
+    cout << 10.0 / 3.0 << endl;
+
+
+    // Math functions
+
+    cout << pow(2,5) << endl;
+
+    cout << sqrt(36) << endl;
+
+    cout << round(4.7) << endl;
+
+    cout << floor(4.8) << endl;
+
+    cout << fmax(5,11) << endl;
+ 
+    cout << fmin(5,11) << endl;
+
+
+    return 0;
+
+}
