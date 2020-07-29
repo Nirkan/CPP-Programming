2ec3dd0a0af5dc543b506cdcad001016d5f84898
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-29 09:42:36 +0200

    Arrays

---
+++ freeCode-CPP/T10.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+
+	int luckyNums[20] = {4, 8, 15, 16, 23, 42};
+
+	cout << luckyNums[0] << endl;
+
+	luckyNums[0] = 19;
+
+	cout << luckyNums[0] << endl;
+
+	luckyNums[10] = 100;
+
+	cout << luckyNums[10] << endl;
+
+
+
+	return 0;
+}
