0f7750df7589ea8fa6b7f84fe3a94728ace23629
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-29 16:28:48 +0200

    while-loop

---
+++ freeCode-CPP/T17.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	int index = 1;
+
+	while(index <= 5){
+
+		cout << "index : " << index << endl;
+
+		index++ ;
+
+	}
+
+
+
+	int pair = 1;
+
+	do{
+
+		cout << "pair : "<< pair << endl;
+
+		pair++ ;
+
+	} while(pair <= 4);
+
+
+
+	return 0;
+
+
+
+	
+
+}
